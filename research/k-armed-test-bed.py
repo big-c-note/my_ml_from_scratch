5e1146327e0495cb82358d0189e3e397c543e6d0
big-c-note <colin@colinmanko.com>
2020-09-27 22:11:29 -0400

    adding multiprocessing and random choice of equal maxes.

--- research/k-armed-test-bed.py
+++ research/k-armed-test-bed.py
@@ -1,48 +1,66 @@
 from typing import Dict, List, Tuple
 import random
+import multiprocessing
 
 import numpy as np
+from joblib import Parallel, delayed
 import matplotlib.pyplot as plt
-from tqdm import trange
+from tqdm import tqdm
+
+
+def _run_test(
+    n_actions: int,
+    n_outer_iters: int,
+    n_inner_iters: int,
+):
+    avg_reward: np.ndarray = np.zeros(n_inner_iters,)
+    rewards: np.ndarray = np.random.normal(0, 1, n_actions)
+    # The index will be the action chosen.
+    q_actions: np.ndarray = np.zeros(n_actions,)
+    num_actions: Dict[int, int] = {}
+    for t in range(n_inner_iters):
+        greedy_actions: np.array = np.argwhere(
+            q_actions == np.max(q_actions)
+        ).reshape(-1)
+        greedy_action: int = np.random.choice(greedy_actions, 1)[0]
+        # It's unlikely that the action-value estimates (Q(at)) would have
+        # multiple tied for best. However, if they did, we may want to break
+        # the tie at random. Of course, it would be more efficeint to
+        # only do that if multiple actions are retuened.
+        if np.random.rand(1)[0] < epsilon:
+            explore_options: List[int] = [
+                x for x in range(10) if x != greedy_action
+            ]
+            a_t: int = np.random.choice(explore_options, 1)[0]
+        else:
+            a_t = greedy_action
+        r_t: float = np.random.normal(rewards[a_t], 1, 1)[0]
+        # The reward comes from the normal distribution with expectation of the
+        # randomly generated reward value and a sigma of 1.
+        try:
+            num_actions[a_t] += 1
+        except KeyError:
+            num_actions[a_t] = 1
+        q_action: float = 1 / num_actions[a_t] * (r_t - q_actions[a_t])
+        q_action = 1 / num_actions[a_t] * (r_t - q_actions[a_t])
+        q_actions[a_t] += q_action
+        avg_reward[t] += r_t / n_outer_iters
+    return avg_reward
 
 
 if __name__ == "__main__":
+    num_cores: int = multiprocessing.cpu_count()
     n_actions: int = 10
     epsilons: List[float] = [0, .001, .003, .01, .03, .1, .3]
     n_outer_iters: int = 2000
     n_inner_iters: int = 1000
-    for epsilon in epsilons:
-        avg_reward: np.ndarray = np.zeros(n_inner_iters,)
-        for _ in trange(n_outer_iters):
-            rewards: np.ndarray = np.random.normal(0, 1, n_actions)
-            # The index will be the action chosen.
-            q_actions: np.ndarray = np.zeros(n_actions,)
-            num_actions: Dict[int, int] = {}
-            for t in range(n_inner_iters):
-                greedy_action: int = q_actions.argmax()
-                # It's unlikely that the action-value estimates (Q(at)) would have
-                # multiple tied for best. However, if they did, we may want to break
-                # the tie at random. I believe argmax just takes the 1st index with the
-                # max.
-                if np.random.rand(1)[0] < epsilon:
-                    explore_options: List[int] = [
-                        x for x in range(10) if x != greedy_action
-                    ]
-                    a_t: int = np.random.choice(explore_options, 1)[0]
-                else:
-                    a_t = greedy_action
-                r_t: float = np.random.normal(rewards[a_t], 1, 1)[0]
-                # The reward comes from the normal distribution with expectation of the
-                # randomly generated reward value and a sigma of 1.
-                try:
-                    num_actions[a_t] += 1
-                except KeyError:
-                    num_actions[a_t] = 1
-                q_action: float = 1 / num_actions[a_t] * (r_t - q_actions[a_t])
-                q_action = 1 / num_actions[a_t] * (r_t - q_actions[a_t])
-                q_actions[a_t] += q_action
-                avg_reward[t] += r_t / n_outer_iters
-
+    for epsilon in tqdm(epsilons):
+        avg_rewards_lst: List[np.ndarray] = Parallel(n_jobs=num_cores)(
+            delayed(_run_test)(
+                n_actions, n_outer_iters, n_inner_iters,
+            ) for _ in range(n_outer_iters)
+        )
+        avg_reward: np.ndarray = sum(avg_rewards_lst)
         rgb: Tuple[float, float, float] = (
             random.random(), random.random(), random.random()
         )
